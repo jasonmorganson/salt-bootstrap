ab284d2a0ecb5af0f1c7b43b192bd4e23f597bf5
Jason Morganson <jason@morganson.me>
2013-08-13 14:47:49 -0400

    Do some cleanup and call salt

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,11 @@
 #
 #   wget -O - https://raw.github.com/jasonmorganson/salt-bootstrap/master/bootstrap.sh | sh
 
-wget https://raw.github.com/jasonmorganson/salt-bootstrap/master/master -P salt-bootstrap/
+wget https://raw.github.com/jasonmorganson/salt-bootstrap/master/master -P salt-bootstrap
 
 wget -O - http://bootstrap.saltstack.org | sudo sh -s -- -M -c salt-bootstrap
+
+rm -r salt-bootstrap
+
+sudo salt-call state.highstate
 
