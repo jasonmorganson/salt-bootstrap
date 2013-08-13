b5c59249566e4f93fd7130ba7f4f66d58088b66d
Jason Morganson <jason@morganson.me>
2013-08-13 14:44:29 -0400

    Include direct command

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env sh
 #
 #
+# Run with the following command:
+#
+#
+#   wget -O - https://raw.github.com/jasonmorganson/salt-bootstrap/master/bootstrap.sh | sh
 
 wget https://raw.github.com/jasonmorganson/salt-bootstrap/master/master -P salt-bootstrap/
 
